46f33f54625ed1fd92db4e9719f4292a46bbbbf0
SylvesterAssiamah <122726691+AssiamahS@users.noreply.github.com>
2024-03-29 00:31:55 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,25 +1,27 @@
 
 
-const myForm = document.querySelector('#myForm')
-function addForm(eventObj){
-    eventObj.preventDefault()
+const myForm = document.querySelector('#myForm');
+function addForm(eventObj) {
+    eventObj.preventDefault();
 
-const BlogPost = JSON.parse(localStorage.getItem('key')) || [];
+const BlogPosts = JSON.parse(localStorage.getItem('key')) || [];
     const inputBlog = {
         username: myForm.username.value,
         blogContent: myForm.querySelector('#blogContent').value,
-        blogTitle: myForm.querySelector('#blogTitle').value
-    }
-    BlogPost.push(inputBlog)
-    console.log(inputBlog);
+        blogTitle: myForm.querySelector('#blogTitle').value };
 
+    BlogPosts.push(inputBlog);
+    // Store the updated BlogPosts array in localStorage
+    localStorage.setItem('key', JSON.stringify(BlogPosts));
+
+    console.log(inputBlog);
     // Storing blog post data in localStorage
    
-    localStorage.setItem('key', JSON.stringify(inputBlog));
-}
-    // Retrieving blog post data from localStorage
-    const retrievedPost = JSON.parse(localStorage.getItem('key'));
-    console.log(retrievedPost);
+    myForm.addEventListener("submit", addForm)
+
+// Retrieving blog post data from localStorage
+const retrievedPosts = JSON.parse(localStorage.getItem('key')) || [];
+    console.log(retrievedPosts);}
 
 
     myForm.addEventListener("submit", addForm)
